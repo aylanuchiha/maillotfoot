52bd3fd9bb3f57c3a343b1b925d185c3e02dbe38
aylan <aylan.lounici1@etu.univ-lorraine.fr>
2025-04-21 19:36:22 +0200

    bdd

--- php/test_db.php
+++ php/test_db.php
@@ -1,5 +1,12 @@
 <?php
-require_once 'php/bdd.php';
+
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+require_once 'bdd.php';
+
 
 try {
     // Si ce code s'exécute, la connexion a réussi
